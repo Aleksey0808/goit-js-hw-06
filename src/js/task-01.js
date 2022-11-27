8032b07fbdaa40e8b22e6b235be29ec8bbc22040
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2022-11-27 22:45:40 +0200

    271122

---
+++ src/js/task-01.js
@@ -0,0 +1,25 @@
+const itemEl = document.querySelectorAll('.item');
+
+console.log(`Number of categories: ${itemEl.length}`)
+
+const nameEl = document.querySelectorAll('h2');
+
+console.log(nameEl[0].textContent)
+
+const ulEl = document.querySelectorAll('.item ul');
+
+console.log(ulEl[0].children.length)
+
+console.log(nameEl[1].textContent)
+
+console.log(ulEl[1].children.length)
+
+console.log(nameEl[2].textContent)
+
+console.log(ulEl[2].children.length)
+
+
+
+
+
+
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+console.log(ingredients)
+
+
+const ingredientsEl = document.querySelector('#ingredients')
+
+const elements = ingredients.map(item => {
+  const liEl = document.createElement('li');
+  liEl.classList.add('item');
+  liEl.textContent = item;
+  return liEl;
+})
+
+console.log(elements)
+
+ingredientsEl.append(...elements)
